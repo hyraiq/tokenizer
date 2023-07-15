2103a19fda7b58a1fe320bf92f6b241de04400e5
Tom Newby <me@tomnewby.net>
2023-07-15 10:29:25 +1000

    feat(tokenizer): Implement first tokenizer

--- src/tokenizer.spec.ts
+++ src/tokenizer.spec.ts
@@ -1,7 +1,61 @@
+import { globalTokens } from './__fixtures/global';
 import { Tokenizer } from './tokenizer';
 
 describe('tokenizer', () => {
-  it('should return world', () => {
-    expect(Tokenizer.hello()).toBe('world');
+  it('should resolve colors', () => {
+    const semantic = {
+      'default': 'white',
+      'secondary': 'grey-25',
+      'hover': 'grey-50',
+      'active': 'grey-100',
+      'selected': 'grey-75',
+      'tertiary': 'grey-50',
+      'tertiary-hover': 'grey-75',
+      'tertiary-active': 'grey-200',
+      'tertiary-selected': 'grey-100',
+      'brand': 'blue-600',
+      'inverse': 'grey-800',
+      'inverse-hover': 'grey-700',
+      'selected-brand': 'blue-50',
+    };
+    expect(Tokenizer.with(globalTokens).handle(semantic)).toEqual({
+      'default': '#FFFFFF',
+      'secondary': '#F9F9FB',
+      'hover': '#F0F1F5',
+      'active': '#D9DBE3',
+      'selected': '#E4E6EC',
+      'tertiary': '#F0F1F5',
+      'tertiary-hover': '#E4E6EC',
+      'tertiary-active': '#C2C6D1',
+      'tertiary-selected': '#D9DBE3',
+      'brand': '#002FFF',
+      'inverse': '#2D3340',
+      'inverse-hover': '#3F4655',
+      'selected-brand': '#EBF3FE',
+    });
+  });
+
+  it('throws if reference attempts to use an object color without specifying a property', () => {
+    expect(() => Tokenizer.with(globalTokens).handle({
+      'foo': 'blue'
+    })).toThrowError(/^Resolved 'blue', however it is an object/);
+  });
+
+  it('throws if reference attempts to access a property on a string', () => {
+    expect(() => Tokenizer.with(globalTokens).handle({
+      'foo': 'white-200'
+    })).toThrowError(/^Tried to resolve 'white-200', but 'white' is a single value instead of an object/);
+  });
+
+  it('throws if cannot find unknown top string token', () => {
+    expect(() => Tokenizer.with(globalTokens).handle({
+      'foo': 'blarg'
+    })).toThrowError(/^Tried to resolve 'blarg', but it is neither a token itself, nor a nested reference/);
+  });
+
+  it('throws if cannot find unknown top nested object', () => {
+    expect(() => Tokenizer.with(globalTokens).handle({
+      'foo': 'blarg-104'
+    })).toThrowError(/^Tried to resolve 'blarg-104', but 'blarg' is not a token/);
   });
 });
--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -1,5 +1,51 @@
+type GlobalTokens = Record<string|number, string|Record<string|number, string>>;
+
 export class Tokenizer {
-  public static hello() {
-    return 'world';
+  public static with(globalTokens: GlobalTokens)  {
+    return new Tokenizer(globalTokens);
+  }
+
+  constructor(private readonly globalTokens: GlobalTokens) {
+  }
+
+  public handle(semanticTokens: Record<string, string>) {
+    return Object.entries(semanticTokens).reduce((previous, current)  => {
+      const resolvedValue = Tokenizer.resolve(current[1], this.globalTokens);
+      return {
+        ...previous,
+        [current[0]]: resolvedValue
+      };
+    }, {} as Record<string, string>);
+  }
+
+  private static resolve(input: string, globalTokens: GlobalTokens) : string {
+    if (globalTokens.hasOwnProperty(input)) {
+      const exactMatch = globalTokens[input];
+      if (typeof exactMatch === 'object') {
+        throw new Error(`Resolved '${input}', however it is an object. Should you be using one of the values from this object?`);
+      }
+
+      return exactMatch;
+    }
+
+    const splitByHyphen = input.match(/(^[^\-]+)(?:\-)(.*)$/);
+    if (splitByHyphen === null) {
+      throw new Error(`Tried to resolve '${input}', but it is neither a token itself, nor a nested reference`);
+    }
+
+    const subkey = splitByHyphen[1];
+    const rest = splitByHyphen[2];
+
+    if (!globalTokens.hasOwnProperty(subkey)) {
+      throw new Error(`Tried to resolve '${input}', but '${subkey}' is not a token`);
+    }
+
+    const subset = globalTokens[subkey];
+
+    if (typeof subset === "string") {
+      throw new Error(`Tried to resolve '${input}', but '${subkey}' is a single value instead of an object`);
+    }
+
+    return this.resolve(rest, subset);
   }
 }
